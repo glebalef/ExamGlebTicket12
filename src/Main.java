12c15134b30367e77f2434c9235644611e2586bb
Gleb <gleb.alef@gmail.com>
2023-01-16 16:57:36 +0000

    задача решена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<User> list = new ArrayList<User>();
+
+        User vasya = new User("vasya", 20);
+        User petya = new User("petya", 31);
+        User sveta = new User("sveta", 18);
+        User sveta2 = new User("sveta", 18);
+        User stepan = new User("stepan", 60);
+        User slava = new User("slavar", 15);
+        User tamara = new User("tamara", 99);
+        User vladimir = new User("vladimir", 15);
+
+        list.add(vasya);
+        list.add(petya);
+        list.add(sveta2);
+        list.add(sveta);
+        list.add(vladimir);
+        list.add(tamara);
+        list.add(slava);
+        list.add(stepan);
+
+        System.out.println(getOldestUser(list).toString());
+    }
+
+    public static User getOldestUser (Collection <User> users) {
+
+       return users.stream()
+                .distinct()
+                .sorted(Comparator.comparing(User::getAge).thenComparing(User::getName))
+                .max((Comparator.comparing(User::getAge)))
+                .orElse(null);
     }
 }
